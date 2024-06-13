515a5e37ec0cd6dc54fe4c87cd1f4310516df486
Felipe Gonzalez <contraviento@gmail.com>
2024-06-12 18:51:02 -0700

    create controllers and route for each endpoint

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
+const productRoutes = require('./routes/product.route.js');
 const app = express();
+const uri = 'mongodb+srv://admin:Maxima%401886@backenddb.oeak0jm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB';
 
+// middleware
 app.use(express.json());
-
-const uri =
-	'mongodb+srv://admin:Maxima%401886@backenddb.oeak0jm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB';
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
-	res.send('Hello from node api server');
+	res.send('Hello from Node Api server');
 });
 
-app.get('/api/products', async (req, res) => {
-	try {
-		const products = await Product.find({});
-		res.status(200).json(products);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
-
-app.post('/api/products', async (req, res) => {
-	try {
-		const product = await Product.create(req.body);
-		res.status(200).json(product);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+app.use('/api/products', productRoutes);
 
 mongoose
 	.connect(uri)
