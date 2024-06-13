28cf319baf9912517f430862456642c424664ce3
Felipe Gonzalez <contraviento@gmail.com>
2024-06-12 21:00:55 -0700

    add env environment to protect ket database

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const productRoutes = require('./routes/product.route.js');
 const app = express();
-const uri = 'mongodb+srv://admin:Maxima%401886@backenddb.oeak0jm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB';
+const dotenv = require('dotenv');
+
+dotenv.config();
+const uri = process.env.MONGODB_URI;
 
 // middleware
 app.use(express.json());
