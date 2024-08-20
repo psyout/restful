afd9622b61b7118809de652227ed51089ba5757a
Felipe Gonzalez <contraviento@gmail.com>
2024-08-19 19:19:50 -0700

    create datebase schema to crud bussiness into the happy hour app

--- models/product.model.js
+++ models/product.model.js
@@ -8,21 +8,55 @@ const ProductSchema = mongoose.Schema(
 			required: [true, 'Please enter a product name'],
 		},
 
-		quantity: {
+		address: {
+			type: String,
+			required: true,
+		},
+
+		addressFull: {
+			type: String,
+			required: true,
+		},
+
+		phone: {
 			type: Number,
 			required: true,
 			default: 0,
 		},
 
-		price: {
-			type: Number,
+		time: {
+			type: [String],
 			required: true,
-			default: 0,
+			default: [],
 		},
 
-		image: {
+		menu: {
+			drinks: {
+				type: [String], // Changed to an array of strings
+				required: true,
+				default: [], // Default is an empty array
+			},
+			food: {
+				type: [String], // Changed to an array of strings
+				required: true,
+				default: [], // Default is an empty array
+			},
+		},
+
+		image_url: {
 			type: String,
 			required: false,
+		},
+
+		coordinates: {
+			latitude: {
+				type: Number,
+				required: true,
+			},
+			longitude: {
+				type: Number,
+				required: true,
+			},
 		},
 	},
 	{
